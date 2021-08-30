57ed9f4417da8e2ec877af989958cc3f45cbee76
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-30 15:23:20 -0700

    Adding Routes(endpoints) to the server snippet

--- express_server.js
+++ express_server.js
@@ -3,12 +3,16 @@ const app = express();
 const PORT = 8080;
 
 const urlDatabase = {
-  "b2xVn2": "http://www.nfl.ca",
+  "b2xVn2": "http://www.nfl.com",
   "9sm5xK": "http://www.canada.ca"
 };
 
 app.get('/', (req, res) => {
   res.send('Hello!');
+});
+
+app.get('/urls.json', (req, res) => {
+  res.json(urlDatabase);
 });
 
 app.listen(PORT, () => {
