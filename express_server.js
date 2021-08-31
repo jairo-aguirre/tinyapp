eb5c07a4a0a5e425866f9f1446279721eebd0e63
Jairo (Burnaby, BC) <jairo.aguirre@protonmail.com>
2021-08-30 19:03:44 -0700

    Installing middleware body-parser and using POST for new url

--- express_server.js
+++ express_server.js
@@ -1,8 +1,12 @@
 const express = require('express');
-const app = express();
+const bodyParser = require('body-parser');
+
 const PORT = 8080;
 
+const app = express();
+
 app.set('view engine', 'ejs');
+app.use(bodyParser.urlencoded({ extended: true }));
 
 const urlDatabase = {
   "b2xVn2": "http://www.nfl.com",
@@ -18,13 +22,18 @@ app.get('/urls', (req, res) => {
   res.render('urls_index', templateVars);
 });
 
-app.get('urls/new', (req, res) => {
+app.get('/urls/new', (req, res) => {
   res.render('urls_new');
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   const templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL] };
   res.render('urls_show', templateVars);
+});
+
+app.post('/urls', (req, res) => {
+  console.log(req.body);
+  res.send('OK');
 });
 
 app.get('/hello', (req, res) => {
